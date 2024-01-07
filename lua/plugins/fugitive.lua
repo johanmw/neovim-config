5a4340fb3b62ae55fc9a026a162affb256eaa327
Johan Wesselink <johan.wesselink@nedap.com>
2024-01-07 23:02:06 +0100

    Forgot to add the fugtiive file

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,8 @@
 return {
 	"tpope/vim-fugitive",
 	config = function() 
-		--vim.keymap,set('n', '<leader>gs', '<cmd> Git<cr>')
+		--vim.keymap.set('n', '<leader>gs', '<cmd> Git<cr>')
+		vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 		print("Init fugitive")
 	end
 }
